911f3c25c4e2bb1a078341a7c9ead25d391cfc29
Kelvin Muza <k1muza@gmail.com>
2025-06-28 02:43:54 +0200

    Add floating edit toggle

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import EditToggle from "@/components/EditToggle";
 
 
 const inter = Inter({ subsets: ['latin'] });
@@ -20,6 +21,7 @@ export default function RootLayout({
         className={inter.className}
       >
         {children}
+        <EditToggle />
       </body>
     </html>
   );
