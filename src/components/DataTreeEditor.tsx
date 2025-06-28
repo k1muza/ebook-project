f32340928c11f44d7eb01ac2100e84d26514e7df
Kelvin Muza <k1muza@gmail.com>
2025-06-28 12:12:14 +0200

    Add JSON tree editor page

---
+++ src/components/DataTreeEditor.tsx
@@ -0,0 +1,33 @@
+'use client'
+import JsonView from 'react18-json-view'
+import 'react18-json-view/src/style.css'
+import { useReport } from '@/contexts/ReportContext'
+import { ReportData } from '@/types/report'
+
+const DataTreeEditor = () => {
+  const { data, setData, save } = useReport()
+
+  if (!data) return <p>Loading...</p>
+
+  const update = (src: unknown) => {
+    const cloned = JSON.parse(JSON.stringify(src)) as ReportData
+    setData(cloned)
+    save()
+  }
+
+  return (
+    <div className="p-4">
+      <JsonView
+        src={data}
+        editable={{ add: true, edit: true, delete: true }}
+        onEdit={({ src }) => update(src)}
+        onAdd={({ src }) => update(src)}
+        onDelete={({ src }) => update(src)}
+        style={{ backgroundColor: 'transparent' }}
+        theme="github"
+      />
+    </div>
+  )
+}
+
+export default DataTreeEditor
